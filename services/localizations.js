51ed479def5e7731ed41bb9df987afa598843aff
Christian Dallago <code@dallago.us>
2017-04-11 14:21:13 +0200

    removed unused code

--- services/localizations.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(context) {
-    const localizationsController = context.component('controllers').module('localizations');
-    context.api
-        .get('/localizations/uniprotId/:id', localizationsController.getProteinByUniprotId)
-        .get('/localizations/search/:id', localizationsController.getProteinsLocalizations);
-}
--- services/mappings.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(context) {
-    const mappingsController = context.component('controllers').module('mappings');
-    context.api
-        .get('/mappings/uniprot/:id', mappingsController.getProteinMappingByUniProtID)
-        .get('/mappings/search/:id', mappingsController.getProteinsMappings);
-}
